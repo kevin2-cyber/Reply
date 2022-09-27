ce804fedf53af51a902cd2b588604a61b665c11d
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-27 03:45:30 +0000

    new

--- app/src/main/java/io/materialstudies/reply/MainActivity.kt
+++ app/src/main/java/io/materialstudies/reply/MainActivity.kt
@@ -2,10 +2,28 @@ package io.materialstudies.reply
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+                    Toolbar.OnMenuItemClickListener,
+                    NavController.OnDestinationChangedListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDestinationChanged(
+        controller: NavController,
+        destination: NavDestination,
+        arguments: Bundle?
+    ) {
+        TODO("Not yet implemented")
     }
 }
