458597c0486603edf153b3553b87a393e5e62fa3
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-29 00:29:08 +0000

    ObsoleteSdkint

--- app/src/main/java/io/materialstudies/reply/util/ViewExtensions.kt
+++ app/src/main/java/io/materialstudies/reply/util/ViewExtensions.kt
@@ -1,9 +1,11 @@
 package io.materialstudies.reply.util
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Build
 import android.widget.TextView
 
+@SuppressLint("ObsoleteSdkInt")
 @Suppress("DEPRECATION")
 fun TextView.setTextAppearanceCompat(
     context: Context,
