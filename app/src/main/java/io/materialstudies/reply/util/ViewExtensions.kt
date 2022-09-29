e1876ffe8744149f8f87cc4b93e56f48a68068cd
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-29 00:25:49 +0000

    deprecation

--- app/src/main/java/io/materialstudies/reply/util/ViewExtensions.kt
+++ app/src/main/java/io/materialstudies/reply/util/ViewExtensions.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.os.Build
 import android.widget.TextView
 
+@Suppress("DEPRECATION")
 fun TextView.setTextAppearanceCompat(
     context: Context,
     resId: Int
