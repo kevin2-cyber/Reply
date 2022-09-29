0fa1e8cbae1cda5d5855fd459e4fb0751d1a7f64
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-29 05:44:34 +0000

    this is where I could reach

--- app/src/main/java/io/materialstudies/reply/ui/search/SearchFragment.kt
+++ app/src/main/java/io/materialstudies/reply/ui/search/SearchFragment.kt
@@ -5,56 +5,69 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.StringRes
+import androidx.navigation.fragment.findNavController
 import io.materialstudies.reply.R
+import io.materialstudies.reply.data.SearchSuggestion
+import io.materialstudies.reply.data.SearchSuggestionStore
+import io.materialstudies.reply.databinding.FragmentSearchBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SearchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding: FragmentSearchBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+        // TODO: Set up MaterialSharedAxis transition as exit and reenter transitions.
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    ): View {
+        binding = FragmentSearchBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.searchToolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
+        setUpSuggestions(binding.searchSuggestionContainer)
+    }
+
+    private fun setUpSuggestions(suggestionContainer: ViewGroup) {
+        addSuggestionTitleView(suggestionContainer, R.string.search_suggestion_title_yesterday)
+        addSuggestionItemViews(suggestionContainer, SearchSuggestionStore.YESTERDAY_SUGGESTIONS)
+        addSuggestionTitleView(suggestionContainer, R.string.search_suggestion_title_this_week)
+        addSuggestionItemViews(suggestionContainer, SearchSuggestionStore.THIS_WEEK_SUGGESTIONS)
+    }
+
+    private fun addSuggestionTitleView(
+        parent: ViewGroup,
+        @StringRes titleResId: Int
+    ) {
+       val inflater = LayoutInflater.from(parent.context)
+       val titleBinding = SearchSuggestionTitleBinding.inflate(inflater, parent, false)
+        titleBinding.title = titleResId
+        parent.addView(titleBinding.root)
+    }
+
+    private fun addSuggestionItemViews(
+        parent: ViewGroup,
+        suggestions: List<SearchSuggestion>
+    ) {
+        suggestions.forEach {
+            val inflater = LayoutInflater.from(parent.context)
+            val suggestionBinding = SearchSuggestionItemBinding.inflate(inflater, parent, false)
+            suggestionBinding.suggestion = it
+            parent.addView(suggestionBinding.root)
+        }
     }
 }
