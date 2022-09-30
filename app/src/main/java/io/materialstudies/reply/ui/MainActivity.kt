61e8cd20c01e901f5af95714e72881a137c905ec
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-30 03:29:37 +0000

    new

--- app/src/main/java/io/materialstudies/reply/ui/MainActivity.kt
+++ app/src/main/java/io/materialstudies/reply/ui/MainActivity.kt
@@ -7,13 +7,18 @@ import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
 import io.materialstudies.reply.R
+import io.materialstudies.reply.databinding.ActivityMainBinding
+import io.materialstudies.reply.ui.nav.BottomNavDrawerFragment
+import io.materialstudies.reply.util.contentView
+import kotlin.LazyThreadSafetyMode.*
 
 class MainActivity : AppCompatActivity(),
                     Toolbar.OnMenuItemClickListener,
                     NavController.OnDestinationChangedListener{
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+    private val binding: ActivityMainBinding by contentView(R.layout.activity_main)
+    private val bottomNavDrawer: BottomNavDrawerFragment by lazy(NONE) {
+        supportFragmentManager.findFragmentById(R.id.bottom_nav_drawer) as BottomNavDrawerFragment
     }
 
     override fun onMenuItemClick(item: MenuItem?): Boolean {
