f9dc591995ab05af489910bfbcf9e3ca32f86e8d
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-30 02:30:19 +0000

    done setting up the HomeFragment

--- app/src/main/java/io/materialstudies/reply/ui/home/HomeFragment.kt
+++ app/src/main/java/io/materialstudies/reply/ui/home/HomeFragment.kt
@@ -1,60 +1,100 @@
 package io.materialstudies.reply.ui.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.ItemTouchHelper
 import io.materialstudies.reply.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import io.materialstudies.reply.data.Email
+import io.materialstudies.reply.data.EmailStore
+import io.materialstudies.reply.databinding.FragmentHomeBinding
+import io.materialstudies.reply.ui.MainActivity
+import io.materialstudies.reply.ui.MenuBottomSheetDialogFragment
+import io.materialstudies.reply.ui.nav.NavigationModel
 
 /**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
+ * A [Fragment] that displays a list of emails.
  */
-class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class HomeFragment : Fragment(), EmailAdapter.EmailAdapterListener {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private val args: HomeFragmentArgs by navArgs()
+
+    private lateinit var binding: FragmentHomeBinding
+
+    private val emailAdapter = EmailAdapter(this)
+
+    // An on back pressed callback that handles replacing any non-Inbox HomeFragment with inbox
+    // on back pressed.
+    private val nonInboxOnBackCallback = object : OnBackPressedCallback(false) {
+        override fun handleOnBackPressed() {
+            NavigationModel.setNavigationMenuItemChecked(NavigationModel.INBOX_ID)
+            (requireActivity() as MainActivity)
+                .navigateToHome(R.string.navigation_inbox, Mailbox.INBOX);
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Set up MaterialFadeThrough enterTransition.
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // TODO: Set up postponed enter transition.
+
+        // Only enable the on back callback if this home fragment is a mailbox other than Inbox.
+        // This is to make sure we always navigate back to Inbox before exiting the app.
+        nonInboxOnBackCallback.isEnabled = args.mailbox != Mailbox.INBOX
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            nonInboxOnBackCallback
+        )
+
+        binding.recyclerView.apply {
+            val itemTouchHelper = ItemTouchHelper(ReboundingSwipeActionCallback())
+            itemTouchHelper.attachToRecyclerView(this)
+            adapter = emailAdapter
+        }
+        binding.recyclerView.adapter = emailAdapter
+
+        EmailStore.getEmails(args.mailbox).observe(viewLifecycleOwner) {
+            emailAdapter.submitList(it)
+        }
+    }
+
+    override fun onEmailClicked(cardView: View, email: Email) {
+        // TODO: Set up MaterialElevationScale transition as exit and reenter transitions.
+        val directions = HomeFragmentDirections.actionHomeFragmentToEmailFragment(email.id)
+        findNavController().navigate(directions)
+    }
+
+    override fun onEmailLongPressed(email: Email): Boolean {
+        MenuBottomSheetDialogFragment
+            .newInstance(R.menu.email_bottom_sheet_menu)
+            .show(parentFragmentManager, null)
+
+        return true
+    }
+
+    override fun onEmailStarChanged(email: Email, newValue: Boolean) {
+        EmailStore.update(email.id) { isStarred = newValue }
+    }
+    override fun onEmailArchived(email: Email) {
+        EmailStore.delete(email.id)
     }
 }
